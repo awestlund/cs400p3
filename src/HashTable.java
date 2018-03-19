15992614d8489cd62a556d6b94650737ec41c77b
S <scuhsoine@users.noreply.github.com>
2018-03-19 16:52:17 -0500

    updated hashtable, ready for submission

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,12 +1,50 @@
 
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title:           HashTable
+// Files:           AnalysisTest.java, HashTable.java, HashTableADT.java, 
+//			PerformanceAnalysis.java, PerformanceAnalysisHash.java
+// Course:          CS400
+//
+// Author:          Susie Chongthaweephol
+// Email:           chongthaweep@wisc.edu
+// Lecturer's Name: Debra Deppeler
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name:    Amber Westlund
+// Partner Email:   awestlund@wisc.edu
+// Lecturer's Name: Debra Deppeler
+// 
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//   _X_ Write-up states that pair programming is allowed for this assignment.
+//   _X_ We have both read and understand the course Pair Programming Policy.
+//   _X_ We have registered our team prior to the team registration deadline.
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully 
+// acknowledge and credit those sources of help here.  Instructors and TAs do 
+// not need to be credited here, but tutors, friends, relatives, room mates 
+// strangers, etc do.  If you received no outside help from either type of 
+// source, then please explicitly indicate NONE.
+//
+// Persons:         none
+// Online Sources:  none
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 import java.util.Iterator;
 import java.util.LinkedList;
 
-//https://secweb.cs.odu.edu/~zeil/cs330/live/website/Slides/libraries/Hashtable.java.html
-//http://homepage.divms.uiowa.edu/~kvaradar/sp2009/SeparateChainingHashTable.java
-
+/**
+ * This class implements a HashTable
+ *
+ * Bugs: none that we are aware of.
+ *
+ * @author Amber
+ * @author Susie
+ */
 public class HashTable<K, V> implements HashTableADT<K, V> {
-
 	/*
 	 * Instance variables and constructors
 	 */
@@ -16,17 +54,14 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 	protected LinkedList<K>[] expanded_key;
 	protected LinkedList<V>[] value_table; // array of type linked list
 	protected LinkedList<K>[] key_table;
-	// how to handle collisions? -> linked lists
 
 	public HashTable() {
-		// constructor
-
 		this.table_size = bestPrime(4);
 		this.LOAD_FACTOR = 0.75;
 		value_table = (LinkedList<V>[]) new LinkedList[table_size]; // our hash table
 		expanded = (LinkedList<V>[]) new LinkedList[table_size];
 		key_table = (LinkedList<K>[]) new LinkedList[table_size]; // our hash table
-		expanded_key =  (LinkedList<K>[]) new LinkedList[table_size];
+		expanded_key = (LinkedList<K>[]) new LinkedList[table_size];
 		for (int j = 0; j < table_size; j++) {
 			value_table[j] = new LinkedList<V>();
 			key_table[j] = new LinkedList<K>();
@@ -42,7 +77,7 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 		value_table = (LinkedList<V>[]) new LinkedList[table_size]; // our hash table
 		expanded = (LinkedList<V>[]) new LinkedList[table_size];
 		key_table = (LinkedList<K>[]) new LinkedList[table_size]; // our hash table
-		expanded_key =  (LinkedList<K>[]) new LinkedList[table_size];
+		expanded_key = (LinkedList<K>[]) new LinkedList[table_size];
 		for (int j = 0; j < table_size; j++) {
 			value_table[j] = new LinkedList<V>();
 			key_table[j] = new LinkedList<K>();
@@ -53,6 +88,7 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 
 	@Override
 	/**
+	 * Inserts a key and a value into the HashTable
 	 *
 	 * @param key
 	 *            : The key that goes into the hashtable
@@ -61,12 +97,7 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 	 * @return value of the key added to the hashtable, throws NullPointerException
 	 *         if key is null
 	 */
-	public V put(K key, V value) { 
-		// TODO: Implement put method - using efficient algorithm
-		// update array size if needed
-		// check load factor ^^
-		// rehash if needed
-
+	public V put(K key, V value) {
 		// find index
 		// see if value is a duplicate
 		if (value == null || key == null) {
@@ -74,7 +105,7 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 		} else {
 			try {
 				if (findKey(key) == true) {
-					return null; // maybe?
+					return null;
 				}
 
 				if ((size() / table_size) > LOAD_FACTOR) {
@@ -85,7 +116,7 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 				}
 				int index = hash(key) % value_table.length;
 				// put in array at the index, if index already has values, add to the end of the
-				// linked list?
+				// linked list
 				value_table[index].add(value); // add to the array in the correct linked list
 				expanded[index].add(value);
 				key_table[index].add(key); // add to the array in the correct linked list
@@ -98,23 +129,27 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 	}
 
 	@Override
+	/**
+	 * Clear the hashtable of all its contents
+	 */
 	public void clear() {
-		// TODO: Implement this method
 		// go through array and set all values to null
 		value_table = (LinkedList<V>[]) new LinkedList[table_size]; // our hash table
 		expanded = (LinkedList<V>[]) new LinkedList[table_size];
 		key_table = (LinkedList<K>[]) new LinkedList[table_size]; // our hash table
-		expanded_key =  (LinkedList<K>[]) new LinkedList[table_size];
-//		for (int i = 0; i < table_size; i++) {
-//			value_table[i].clear();
-//			//other arrays
-//		}
-		//
+		expanded_key = (LinkedList<K>[]) new LinkedList[table_size];
 	}
 
 	@Override
+	/**
+	 * Searches for a key in the HashTable
+	 * 
+	 * @param key:
+	 *            The key for which the value is returned
+	 * @return The value associated with the key, else throws NoSuch Element
+	 *         Exception
+	 */
 	public V get(K key) {
-		// TODO: Implement the get method
 		int index = hash(key) % table_size;
 		// key equal to search key: search hit
 		if (key_table[index] != null) {
@@ -126,9 +161,6 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 				if (it.next() != key) {
 					count++;
 				}
-				// (V) it.next(); // wrong need "value" to know what node it would be?
-				// or should we just return the first value in the linked list
-				// not sure here either.
 			}
 			int count2 = 0;
 			Iterator<V> it2 = value_table[index].iterator();
@@ -146,8 +178,12 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 	}
 
 	@Override
-	public boolean isEmpty() { // me
-		// TODO: Implement the method
+	/**
+	 * Checks if the hashtable is empty
+	 * 
+	 * @return true : if Empty, else False
+	 */
+	public boolean isEmpty() {
 		// go through the whole table to see if any value is entered
 		int i = 0;
 		while (i < table_size) {
@@ -163,6 +199,8 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 
 	/**
 	 *
+	 * Removes a key from the HashTable
+	 *
 	 * @param key:
 	 *            Key of the entry to be removed
 	 * @return value: Value of the key-value pair removed, null if key did not have
@@ -172,30 +210,28 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 	 */
 	@Override
 	public V remove(K key) {
-		// TODO: Implement the remove method
 		int index = hash(key) % table_size;
 		if (key_table[index] != null) {
 			int count = 0;
 			Iterator<K> it = key_table[index].iterator();
 			while (it.hasNext()) {
-				// compare key values and return that same index in array and linked list in the
-				// value_table
+				// compare key values and return that same index in array
+				// and linked list in the value_table
 				if (it.next() != key) {
 					count++;
 				}
 			}
-	
+
 			int count2 = 0;
 			Iterator<V> it2 = value_table[index].iterator();
 			while (it2.hasNext()) {
 				V ret_this = it2.next();
 				if (count2 == count) {
-					//V ret_this = it2.next();
 					// if the correct value is there, delete it
 					if (value_table[index].contains(ret_this)) {
-							key_table[index].remove(key);
-							value_table[index].remove(ret_this);
-							return ret_this;
+						key_table[index].remove(key);
+						value_table[index].remove(ret_this);
+						return ret_this;
 					}
 				}
 				count2++;
@@ -206,8 +242,11 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 	}
 
 	@Override
-	public int size() { // me
-		// TODO: Implement this method
+	/**
+	 *
+	 * @return: The total number of entries in the hashtable
+	 */
+	public int size() {
 		int count = 0;
 		// go through the whole table to see if any value is entered
 		for (int i = 0; i < table_size; i++) {
@@ -221,39 +260,42 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 		return count;
 	}
 
+	/**
+	 * Private helper method for hash function for keys
+	 * 
+	 * @param key:
+	 *            the key of interest
+	 * @return: the hashcode of the key
+	 */
 	private int hash(K key) {
-		// get the hash key
 		return key.hashCode();
 	}
 
-		private void rehash() {
-		// int oldCap = table.length;
+	/**
+	 * Private helper method that rehashes the table into a larger one
+	 */
+	private void rehash() {
 		LinkedList<V> oldTable[] = value_table;
 		LinkedList<K> oldTableKey[] = key_table;
 		table_size = bestPrime(table_size * 2);
-//		V value = null;
-//		K key;
 		expand();
-		
 		value_table = expanded;
 		key_table = expanded_key;
-		
-//        table_size = 0;
-        for( int i = 0; i < oldTable.length; i++ ) {
-//            for( V value : oldTable[i] ) {
-//            	put(value);
-//            }
-            	for(K key : oldTableKey[i] ) {
-//            		value = value_table[i];
-            		put(key, get(key));
-            	}
-//            	for( V value : oldTable[i] ) {
-//                	put(hash((K)getKey(value)), value);
-//                }
-        }
-            }
 
-	// tried to do it, let me know what you think
+		for (int i = 0; i < oldTable.length; i++) {
+			for (K key : oldTableKey[i]) {
+				put(key, get(key));
+			}
+		}
+	}
+
+	/**
+	 * Private helper method to find the hashkey
+	 * 
+	 * @param key:
+	 *            the key of interest
+	 * @return: returns true if key is present
+	 */
 	private boolean findKey(K key) {
 		int index = hash(key) % table_size;
 		if (key_table[index] != null) {
@@ -268,7 +310,14 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 		return false;
 	}
 
-	// find next prime number, used for rehash
+	/**
+	 * Private helper method to find the next prime number, used in the rehash
+	 * method.
+	 * 
+	 * @param key:
+	 *            the number n that the array is doubled to
+	 * @return: the next prime number over the number n
+	 */
 	private int bestPrime(int n) {
 		int counter;
 		n++;
@@ -287,8 +336,10 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 		}
 	}
 
+	/**
+	 * Private helper method that expands the array size when the load factor is hit
+	 */
 	private void expand() {
-		// expand the array size when the load factor is hit
 		// double the array size
 		// += old expanded array = new bigger array
 		expanded = (LinkedList<V>[]) new LinkedList[bestPrime(table_size)];
@@ -297,27 +348,7 @@ public class HashTable<K, V> implements HashTableADT<K, V> {
 			expanded[j] = new LinkedList<V>();
 			expanded_key[j] = new LinkedList<K>();
 		}
-		// rehash
-		// replace table array w/ expanded array (named table array)
 
 	}
 
-	private K getKey(int index, K key) {
-		if (key_table[index] != null) {
-			int count = 0;
-			Iterator<K> it_key = key_table[index].iterator();
-			while (it_key.hasNext()) {
-				// compare key values and return that same index in array and linked list in the
-				// value_table
-				if (it_key.next() == key) {
-					return it_key.next();
-				}
-				count++;
-				// (V) it.next(); // wrong need "value" to know what node it would be?
-				// or should we just return the first value in the linked list
-				// not sure here either.
-			}
-		}
-		return null;
-	}
 }
